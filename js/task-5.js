94eb16e2febef296e8e8a20dc29e9b9745edba3d
Dev3773 <stepaniuk.sergiy@gmail.com>
2024-03-31 22:08:26 +0300

    change branch seven-five

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyRef = document.querySelector("body");
+const hexColorRef = document.querySelector(".color");
+const btnChangeColorRef = document.querySelector(".change-color");
+
+const bgrClrBody = (() => {
+  const newHexColor = getRandomHexColor();
+
+  bodyRef.style.backgroundColor = newHexColor;
+  hexColorRef.textContent = newHexColor;
+});
+
+btnChangeColorRef.addEventListener("click", bgrClrBody);
