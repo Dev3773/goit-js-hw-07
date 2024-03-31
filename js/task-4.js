c63e091ec85cb35db34417e8a5310f24f875766e
Dev3773 <stepaniuk.sergiy@gmail.com>
2024-03-31 20:25:57 +0300

    Made task four

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,26 @@
+const formRef = document.querySelector(".login-form");
+
+const handleSubmit = ((event) => {
+  event.preventDefault();
+
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+  
+   const formData = {
+    email,
+    password,
+   };
+  
+  console.log(formData);
+
+  form.reset();
+});
+
+formRef.addEventListener("submit", handleSubmit);
 
