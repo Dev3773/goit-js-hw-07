0fe3ac5dfeaf8d84b8b42341f075e2950b5458be
Dev3773 <stepaniuk.sergiy@gmail.com>
2024-04-03 20:50:26 +0300

    Made layout task six

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,69 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const inputNumber = document.querySelector("input");
+const boxesRef = document.querySelector("#boxes");
+const buttonCreate = document.querySelector("button[data-create]");
+const buttonDestroy = document.querySelector("button[data-destroy]");
+
+let nameInput;
+
+const targetValue = ((event) => {
+  nameInput = event.currentTarget.value;
+});
+
+inputNumber.addEventListener("input", targetValue);
+
+function createBoxes(amount) {
+  if (amount < 1) {
+    alert("Please enter a number greater than or equal to 1.");
+    return;
+  }
+
+  let size = 30;
+ 
+  for (let i = 0; i < amount; i += 1) {
+    const boxCountInput = document.createElement("div");
+    boxCountInput.classList.add("boxes__item");
+    boxCountInput.style.width = `${size}px`;
+    boxCountInput.style.height = `${size}px`;
+    boxCountInput.style.backgroundColor = getRandomHexColor();
+    boxesRef.appendChild(boxCountInput);
+
+    size += 10;
+  }
+}
+
+const numberCreate = (() => {
+  if (nameInput && !isNaN(nameInput)) {
+    createBoxes(nameInput);
+  } else {
+    alert("Please enter a valid number.");
+  }
+});
+
+const destroyBoxes = (() => boxesRef.innerHTML = '');
+
+buttonCreate.addEventListener("click", numberCreate);
+buttonDestroy.addEventListener("click", destroyBoxes);
